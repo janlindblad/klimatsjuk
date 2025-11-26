7ca9f958fb8f603f3bc1a7e1fe009785e98c3809
Jan Lindblad <jan.lindblad@for.eco>
2025-11-26 21:40:26 +0100

    Add planeten page

--- counter/urls.py
+++ counter/urls.py
@@ -5,6 +5,7 @@ app_name = 'counter'
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('planeten/', views.planeten, name='planeten'),
     path('submit/', views.submit_quote, name='submit_quote'),
     path('api/quotes/', views.approved_quotes_api, name='approved_quotes_api'),
 ]

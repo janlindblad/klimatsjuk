bbebf25fe0157eafd09648215650a94d21b870bc
Jan Lindblad <jan.lindblad@for.eco>
2025-12-12 09:25:36 +0100

    Change admin text field to text area

--- counter/admin.py
+++ counter/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 from .models import Quote
 
 
@@ -7,3 +9,6 @@ class QuoteAdmin(admin.ModelAdmin):
     list_display = ('text', 'days_count', 'submission_time', 'approved')
     list_filter = ('approved', 'submission_time')
     search_fields = ('text',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 80})},
+    }
